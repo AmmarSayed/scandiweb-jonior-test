488b32bd8601f8932094220ead9dc834477741e2
Ammar Sayed <ammarsayed1988@gmail.com>
2022-11-11 23:51:24 +0200

    added single product page

---
+++ src/Components/MiniCart/AttributeSingleProduct.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { isSelected } from "../../utils/utils";
+
+export class AttributeSingleProduct extends Component {
+  render() {
+    const { selectedAttributes, attr, select } = this.props;
+    return (
+      <div key={attr.name} className="cart-item__info__attribute">
+        <p className="attribute">{attr.name}:</p>
+        <ul>
+          {attr.items.map((attrItem) => {
+            const listStyle = attr.type === "swatch" ? "color" : "";
+            const content = attr.type === "swatch" ? "" : attrItem.value;
+            const bgColor = attr.type === "swatch" ? attrItem.value : "";
+
+            const isSelectedAttribute = selectedAttributes ? selectedAttributes[attr.name] === attrItem.value : null;
+
+            const classes = `${listStyle} ${isSelectedAttribute ? "active" : null}`;
+            return (
+              <li
+                key={attrItem.value}
+                className={classes}
+                style={{ backgroundColor: bgColor, cursor: "pointer" }}
+                onClick={() => select({ name: attr.name, value: attrItem.value })}
+              >
+                {content}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default AttributeSingleProduct;
